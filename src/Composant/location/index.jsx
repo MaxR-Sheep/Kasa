0d50aebbee0c4ffd96e4dd5d42383d04bc9e3e85
Maxime Ronjat <maxime.ronjat@gmail.com>
2023-04-25 10:24:16 +0200

    création collapse

---
+++ src/Composant/location/index.jsx
@@ -0,0 +1,15 @@
+import logements from "../../datas/logement.json";
+import Card from "../card";
+import "../Styles/card.css";
+
+function Location() {
+  return (
+    <div className="card">
+      {logements.map((obj) => {
+        return <Card product={obj} key={obj.id} />;
+      })}
+    </div>
+  );
+}
+
+export default Location;
