d4a9e47c329dc778dbc979e8383795ecd80a33dc
Maxime Ronjat <maxime.ronjat@gmail.com>
2023-04-26 17:35:13 +0200

    ajout page error avec style

--- src/Composant/Error/index.jsx
+++ src/Composant/Error/index.jsx
@@ -1,8 +1,17 @@
+import { Link } from "react-router-dom";
+import "../../styles/error.css";
+
 function Error() {
   return (
-    <div>
-      <h1>404 </h1>
-      <h2> Cette page n'existe pas</h2>
+    <div className="error">
+      <h1 className="error_404">404 </h1>
+      <h2 className="error_text">
+        {" "}
+        Oups! La page que vous demandez n'existe pas.
+      </h2>
+      <Link to="/" className="error_link">
+        Retourner sur la page d’accueil
+      </Link>
     </div>
   );
 }
