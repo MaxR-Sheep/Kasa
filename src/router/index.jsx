145eb94c06a721e695fd70d4e6924dda51ac6959
Maxime Ronjat <maxime.ronjat@gmail.com>
2023-05-09 11:10:47 +0200

    ajout fichier router

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./styles/index.css";
+import "../styles/index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./app.jsx";
 
